7c433ec9ec764b11ef31ead6d5198b229a8d869e
chuksmili <chukwudimmiliogu0@gmail.com>
2023-10-06 13:20:53 -0700

    alx c programing task completed

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - A c program that prints using simple function
+ *
+ * Return: valid 1 (pass)
+ */
+init main(void)
+{
+	write(2,"and that piece of art is useful" - Dora Korpar, 2015-10-19/n",59);
+	return (1);
+}
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - A programme with proper grammer with the printf function
- * return: 0 (pass)
+ * return: very 0 (pass)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
