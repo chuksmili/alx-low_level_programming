5c84abffc0fbc6aab48d96399708113951893acb
chuksmili <chukwudimmiliogu0@gmail.com>
2023-10-10 21:10:17 -0700

    alx task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _isalpha - checkes for alphabet
+* @c the alphabet to be checked
+* Return: 1 for alphabet or 0 anything else
+*/
+int _isalpha(int c)
+{
+	if ((c>=65 && c<=97) || (c>=97 && c <= 122))
+	{
+		return (1);
+	}
+	return (0);
+}
